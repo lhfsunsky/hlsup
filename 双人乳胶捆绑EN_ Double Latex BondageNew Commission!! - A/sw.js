c06041e5c35b32667dab9a77afd8ac9b92bdc5d4
lhfsunsky <2326423384@qq.com>
2025-08-09 16:14:16 +0800

    2

--- 双人乳胶捆绑EN_ Double Latex BondageNew Commission!! - A/sw.js
+++ 双人乳胶捆绑EN_ Double Latex BondageNew Commission!! - A/sw.js
@@ -2,7 +2,17 @@ const fileMap = {
   'index.m3u8': 'https://i0.hdslb.com/bfs/openplatform/8385deaf5db8341f2eceb36cc3b116fc0ad384a4.txt',
   'segment000.ts': 'https://i0.hdslb.com/bfs/openplatform/98bf17bbd8635dc2121c75b9476efea02a02e440.txt',
 };
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] 安装');
+  // 跳过等待，立刻进入激活阶段
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] 激活');
+  // 立即接管所有页面
+  event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
@@ -48,5 +58,11 @@ self.addEventListener('fetch', event => {
           return fetch(event.request);
         })
     );
+  }
+});
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'skipWaiting') {
+    console.log('[ServiceWorker] 收到 skipWaiting 消息');
+    self.skipWaiting();
   }
 });
