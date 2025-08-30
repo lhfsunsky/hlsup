3c12f3462518101f0d2b1e32465f63bb85a3c434
lhfsunsky <2326423384@qq.com>
2025-08-30 16:52:59 +0800

    8

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-
-    // Pages 会自动提供 public/ 下的文件访问
-    // 这里我们直接 fetch 自身请求
-    const response = await fetch(url.toString());
-
-    // 可以修改响应，例如加 header
-    const headers = new Headers(response.headers);
-    headers.set("X-Custom-Header", "Hello Worker");
-
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers,
-    });
-  },
+  async fetch(request, env) {
+    // Pages 会自动处理 public 下的文件
+    return await env.__STATIC_CONTENT.fetch(request);
+  }
 };
