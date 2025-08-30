fcddb503a7ed0f471b09c5aa6da579c7c50d2cc3
lhfsunsky <2326423384@qq.com>
2025-08-30 18:00:28 +0800

    21

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    const path = url.pathname === "/" ? "index.html" : url.pathname.slice(1);
+    let path = url.pathname === "/" ? "index" : url.pathname.slice(1);
 
-    const file = await env.KV.get(path);
-    if (!file) {
-      return new Response("File not found: " + path, { status: 404 });
-    }
+    // 列出 KV 所有 keys
+    const allKeys = await env.KV.list();
+    // 找到匹配 index 开头的文件
+    const matchedKey = allKeys.keys.find(k => k.name.startsWith(path));
+    if (!matchedKey) return new Response("File not found", { status: 404 });
 
+    const file = await env.KV.get(matchedKey.name);
     return new Response(file, {
       headers: { "Content-Type": "text/html; charset=utf-8" },
     });
   },
 };
+
 
