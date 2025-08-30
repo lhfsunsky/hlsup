c2f815aa89663bd3b7a1fff498d65109d8c6300f
lhfsunsky <2326423384@qq.com>
2025-08-30 17:35:55 +0800

    17

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 export default {
-  async fetch(request, env) {
-    return env.__my-worker-workers_sites_assets.fetch(request);
-  }
-};
+  async fetch(request, env, ctx) {
+    // write a key-value pair
+    await env.KV.put('KEY', 'VALUE');
+
+    // read a key-value pair
+    const value = await env.KV.get('KEY');
+
+    // list all key-value pairs
+    const allKeys = await env.KV.list();
+
+    // delete a key-value pair
+    await env.KV.delete('KEY');
+
+    // return a Workers response
+    return new Response(
+      JSON.stringify({
+        value: value,
+        allKeys: allKeys,
+      }),
+    );
+  } 
+}
 
 
 
