02b72024ce2716fff4fd7e18a2b50ace55f6cba4
lhfsunsky <2326423384@qq.com>
2025-08-30 16:56:05 +0800

    9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export default {
-  async fetch(request, env) {
-    // Pages 会自动处理 public 下的文件
-    return await env.__STATIC_CONTENT.fetch(request);
+  async fetch(request) {
+    return new Response("Hello Worker!", {
+      headers: { "Content-Type": "text/plain" }
+    });
   }
 };
